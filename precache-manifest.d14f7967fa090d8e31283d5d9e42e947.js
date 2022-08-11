b4d3b90bade330635d466072c3f4539fe393a556
thairyo <sonthai1310.works@gmail.com>
2022-08-11 17:19:24 +0700

    Updates

---
+++ precache-manifest.d14f7967fa090d8e31283d5d9e42e947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "03c89e55d12fbb242c78",
+    "url": "/todo-app-redux/static/css/main.682a7902.chunk.css"
+  },
+  {
+    "revision": "03c89e55d12fbb242c78",
+    "url": "/todo-app-redux/static/js/main.03c89e55.chunk.js"
+  },
+  {
+    "revision": "4193776a3a21bfeca0c5",
+    "url": "/todo-app-redux/static/css/1.4064d99d.chunk.css"
+  },
+  {
+    "revision": "4193776a3a21bfeca0c5",
+    "url": "/todo-app-redux/static/js/1.4193776a.chunk.js"
+  },
+  {
+    "revision": "ee6d69eb52a133cd4b49",
+    "url": "/todo-app-redux/static/js/runtime~main.ee6d69eb.js"
+  },
+  {
+    "revision": "bfe56cfd9ca0298750d8e3cfbf04676e",
+    "url": "/todo-app-redux/index.html"
+  }
+];
